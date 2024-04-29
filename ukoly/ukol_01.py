7088783dd82f549f67a26cbe4c15890c0153c5d4
Vendula Benedová <benedovavendula@gmail.com>
2024-04-29 21:37:26 +0200

    class Zamestnanec

--- ukoly/ukol_01.py
+++ ukoly/ukol_01.py
@@ -58,3 +58,46 @@ for zvire in zvirata_dict:
 # assert isinstance(zvire.druh, str)
 # assert isinstance(zvire.vaha, int)
 # assert zvire.export_to_dict() == {'jmeno': 'Láďa', 'druh': 'Koala', 'vaha': 15}
+
+
+### trida Zamestnanec
+
+class Zamestnanec:
+    def __init__(self, cele_jmeno, rocni_plat, pozice):
+        self.cele_jmeno = cele_jmeno
+        self.rocni_plat = rocni_plat
+        self.pozice = pozice
+
+    def __str__(self):
+        return f'{self.cele_jmeno} s ročním platem {self.rocni_plat} Kč pracuje na pozici {self.pozice}.'
+
+    def ziskej_inicialy(self):
+        if ' ' in self.cele_jmeno:
+            return f'{self.cele_jmeno.split()[0][0]}.{self.cele_jmeno.split()[1][0]}.'
+        else:
+            pass
+
+zamestnanci_dict = [
+    {'cele_jmeno': 'Tereza Vysoka', 'rocni_plat': 700_000, 'pozice': 'Cvičitelka tygrů'},
+    {'cele_jmeno': 'Anet Krasna', 'rocni_plat': 600_000, 'pozice': 'Cvičitelka vyder'},
+    {'cele_jmeno': 'Martin Veliky', 'rocni_plat': 650_000, 'pozice': 'Cvičitel ledních medvědů'},
+]
+
+list_zamestnancu = []
+for zamestnanec in zamestnanci_dict:
+    list_zamestnancu.append(Zamestnanec(zamestnanec['cele_jmeno'], zamestnanec['rocni_plat'], zamestnanec['pozice']))
+
+# print(list_zamestnancu)
+# print(list_zamestnancu[0].cele_jmeno)
+# print(list_zamestnancu[1].rocni_plat)
+# print(list_zamestnancu[2].pozice)
+
+# Zamestnanec class
+# zamestnanec = Zamestnanec('Petr Novak', 50000, 'Programator')
+# assert hasattr(zamestnanec, 'cele_jmeno')
+# assert hasattr(zamestnanec, 'rocni_plat')
+# assert hasattr(zamestnanec, 'pozice')
+# assert isinstance(zamestnanec.cele_jmeno, str)
+# assert isinstance(zamestnanec.rocni_plat, int)
+# assert isinstance(zamestnanec.pozice, str)
+# assert zamestnanec.ziskej_inicialy() == 'P.N.'
